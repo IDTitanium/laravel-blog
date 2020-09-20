2a15b115e40283ad7c6175c10be4f588569ba2e8
Idris Lawal <idriseun222@gmail.com>
2020-09-20 19:10:45 +0100

    initial push

---
+++ database/migrations/2020_09_20_151440_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id')->constrained();
+            $table->string('slug');
+            $table->longText('title');
+            $table->longText('subtitle');
+            $table->longText('content_raw');
+            $table->longText('content_html');
+            $table->string('post_image');
+            $table->boolean('is_draft')->default(1);
+            $table->timestamp('published_at')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
